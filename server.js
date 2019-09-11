da1a9f23b75555ddc4cffd08767e6574b1096a42
AjayKhatavkar <ajay@stackmint.com>
2019-09-11 15:20:43 +0530

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./app";
 
 //Use system configuration for port or use 6001 by default.
-const port = process.env.port || 6002;
+const port = process.env.port || 6003;
 
 //Create server with exported express app
 const server = http.createServer(app);
